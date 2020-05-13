4254a338600c2726ba57abeacbbd1fa86e1a2821
Giribabu Nagipogu <giribabu.nagipogu@solutionec.com>
2020-05-13 23:36:52 +0530

    integrations done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Event, Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nodestack-ui';
+  showLoadingIndicator = true;
+  constructor(private _router: Router){
+    this._router.events.subscribe((routerEvent: Event) =>{
+       if(routerEvent instanceof NavigationStart){
+         this.showLoadingIndicator = true;
+       }
+       if(routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationCancel || routerEvent instanceof NavigationError){
+         this.showLoadingIndicator = false;
+       }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ArticleComponent } from './article/article.component';
+import { CreateArticleComponent } from './create-article/create-article.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const appRoutes: Routes = [
+  { 
+    path: 'login', 
+    component: LoginComponent,
+  },
+  { 
+    path: 'register', 
+    component: RegisterComponent,
+  },
+  { 
+    path: 'article', 
+    component: ArticleComponent,
+  },
+  { 
+    path: 'createarticle', 
+    component: CreateArticleComponent,
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ArticleComponent,
+    CreateArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
